3014d2c1a27987a2482cdac2edaa3a3e85bd3c8b
zsoltiNagy <zsoltiusz.nagy@gmail.com>
2017-12-04 12:31:26 +0100

    Wrote basic connection and very basic winforms visibility using S22 library and a sample gmail address.

---
+++ FullRedoneSaintSender/MailClientGUI.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace FullRedoneSaintSender
+{
+    partial class MailClientGUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 47);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(259, 199);
+            this.listBox1.TabIndex = 1;
+            // 
+            // MailClientGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "MailClientGUI";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ FullRedoneSaintSender/MailClientGUI.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+using S22.Imap;
+using System.Net.Mail;
+
+namespace FullRedoneSaintSender
+{
+    public partial class MailClientGUI : Form
+    {
+        public MailClientGUI()
+        {
+            InitializeComponent();
+            string hostname = "imap.gmail.com",
+               username = "CodeCooler123", password = "kalamajka";
+            // The default port for IMAP over SSL is 993.
+            using (ImapClient client = new ImapClient(hostname, 993, username, password, AuthMethod.Login, true))
+            {
+                Console.WriteLine("We are connected!");
+                label1.Text = "We are connected!";
+                // Returns a collection of identifiers of all mails matching the specified search criteria.
+                IEnumerable<uint> uids = client.Search(SearchCondition.Seen());
+                // Download mail messages from the default mailbox.
+                IEnumerable<MailMessage> messages = client.GetMessages(uids);
+                foreach (var message in messages)
+                {
+                    listBox1.Items.Add(message.From.Address);
+                }
+            }
+        }
+    }
+}
